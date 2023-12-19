30748ba9a1605fdc61482fa1cce80027b89a19f4
David C <51928503+dbooyah93@users.noreply.github.com>
2023-12-19 13:45:15 -0600

    Creates a page for Users

---
+++ chapter_e/app/users/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Users',
+}
+
+import React from 'react'
+
+export default function UsersPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
