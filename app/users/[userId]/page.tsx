872e02a8a5a8b4f03d0661ea99606832c520d15f
David C <51928503+dbooyah93@users.noreply.github.com>
2023-12-19 17:57:51 -0600

    async component loads content suspensfuly (broken)

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import getUser from '@/lib/getUser'
 import getUserPosts from '@/lib/getUserPosts'
+import { Suspense } from 'react'
+import Link from 'next/link'
 
 type Params = {
     params: {
@@ -8,13 +10,25 @@ type Params = {
     }
 }
 
-export default function UserPage({ params: { userId }}: Params) {
+export default async function UserPage({ params: { userId }}: Params) {
     const userData: Promise<User> = getUser(userId)
-    const userPosts: Promise<Post[]> = getUserPosts(userId)
+    const userPostsData: Promise<Post[]> = getUserPosts(userId)
 
+    const [user, userPosts] = await Promise.all([userData, userPostsData])
+
+    const user = await userData
+    
     return (
-        <div>
-            
-        </div>
+        <>
+            <h1>User Page</h1>
+            <h2>{user.name}</h2>
+            <br/>
+            <p>
+                <Link href="/Home">Home</Link>
+            </p>
+            <Suspense fallback={<h2>Loading</h2>}>
+                <UserPosts promise={userPosts}></UserPosts>
+            </Suspense>
+        </>
     )
 }
