9b664856c71bf6d4a5dd965998b7186fabe93c1c
David C <51928503+dbooyah93@users.noreply.github.com>
2023-12-19 17:38:16 -0600

    Create getUserPosts for new subpage

---
+++ lib/getUserPosts.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default async function getUserPosts(userId: string) {
+    // Use URL parameter instead of new route because of syntax and clarity
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    if (!res.ok) throw new Error('failed to fetch user');
+    return res.json();
+}
