fb0d137deca85de13f391e8359219f0dfb75d35a
David C <51928503+dbooyah93@users.noreply.github.com>
2023-12-19 17:36:32 -0600

    Create dynamic user content subpage ?userId

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import getUser from '@/lib/getUser'
+import getUserPosts from '@/lib/getUserPosts'
+
 type Params = {
     params: {
         userId: string
     }
 }
 
-export default function UserPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function UserPage({ params: { userId }}: Params) {
+    const userData: Promise<User> = getUser(userId)
+    const userPosts: Promise<Post[]> = getUserPosts(userId)
+
+    return (
+        <div>
+            
+        </div>
+    )
 }
